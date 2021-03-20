67756487a098af44783a2cc0b8e25b1f3d6c463e
alvaropotel <alvaropotel@gmail.com>
2021-03-20 22:11:01 +0000

    Project created

---
+++ dscapone/src/main/java/com/devsuperior/dscapone/DscaponeApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscapone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscaponeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscaponeApplication.class, args);
+	}
+
+}
---
+++ dscapone/src/test/java/com/devsuperior/dscapone/DscaponeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscapone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscaponeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
